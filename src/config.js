c30e40e4ed585e9f36ec739e14cbd13e6da49dfe
jonathanx92 <152698288+jonathanx92@users.noreply.github.com>
2024-06-05 15:21:41 +0200

    .env and config firebase

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const config = {
-    apikey: process.env.FIREBASE_API_KEY,
-    authdomain: process.env.FIREBASE_AUTHDOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    id: process.env.FIREBASE_ID
+    apikey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authdomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    id: process.env.REACT_APP_FIREBASE_ID
   };
 
   
