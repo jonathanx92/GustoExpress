663558520dc54dfa2e67d5b3188bbf95030ecfba
jonathanx92 <152698288+jonathanx92@users.noreply.github.com>
2024-06-08 17:44:11 +0200

    stripe prod

--- src/components/Stripe/index.jsx
+++ src/components/Stripe/index.jsx
@@ -1,39 +1,33 @@
-/* eslint-disable no-undef */
 import React, { useState, useEffect } from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
 import { useCart } from '../Context/CartContext'; 
-import config from "../../config";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC || config.stripePublic);
+// eslint-disable-next-line no-undef
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC);
 
 export default function AppStripe() {
     const [clientSecret, setClientSecret] = useState('');
     const { cart } = useCart(); 
 
     useEffect(() => {
-        const fetchPaymentIntent = async () => {
-            const response = await fetch('https://europe-southwest1-clean-result-424717-b2.cloudfunctions.net/stripepayment', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer ${process.env.REACT_APP_STRIPE_SECRET}`, // Envía la clave secreta en el header
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ items: cart }),
-            });
-
+        fetch('https://europe-southwest1-clean-result-424717-b2.cloudfunctions.net/stripepayment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ items: cart }), 
+        })
+        .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
-
-            const data = await response.json();
-            setClientSecret(data.clientSecret);
-        };
-
-        fetchPaymentIntent().catch(error => console.error('Error fetching client secret:', error));
-    }, [cart]);
-
+            return response.json();
+        })
+        .then(data => setClientSecret(data.clientSecret))
+        .catch(error => console.error('Error fetching client secret:', error));
+    }, [cart]); 
     const options = {
         clientSecret,
     };
